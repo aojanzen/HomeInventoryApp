f6df22d8c221744cd8995bbcbe04b59c249532e7
Andreas Janzen <janzen@gmx.net>
2020-07-30 00:17:20 +0200

    Added main_menu() function

--- HomeInventory.py
+++ HomeInventory.py
@@ -15,3 +15,51 @@ from contextlib import contextmanager
 
 DBNAME = "HomeInventory"
 
+
+def create_room():
+    pass
+
+
+def create_inventory():
+    pass
+
+
+def show_inventory():
+    pass
+
+
+def show_value():
+    pass
+
+
+def main_menu():
+    menu = dict()
+
+    menu["1"] = "Create new room"
+    menu["2"] = "Create new inventory"
+    menu["3"] = "Show inventory list"
+    menu["4"] = "Show total value"
+    menu["5"] = "Quit"
+
+    while True:
+        print("\n")
+        for item, description in sorted(menu.items()):
+            print(f"{item}) {description}")
+        print("\n")
+
+        choice = input("Please chose an action > ")    # add scrub function
+        if choice == "1":
+            create_room()
+        elif choice == "2":
+            create_inventory()
+        elif choice == "3":
+            show_inventory()
+        elif choice == "4":
+            show_value()
+        elif choice == "5":
+            sys.exit()
+        else:
+            print("Invalid input. Please try again!")
+
+
+main_menu()
