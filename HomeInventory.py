746188059e6fa0ccd3e4cd17a00a54bcae198f89
Andreas Janzen <janzen@gmx.net>
2020-08-05 10:31:19 +0200

    Added last missing functions: show_inventory, show_value. Functional test was successful.

--- HomeInventory.py
+++ HomeInventory.py
@@ -107,17 +107,28 @@ def create_inventory(room):
         with access_db() as cursor:
             cursor.execute("INSERT INTO '" + room + "' VALUES(?,?)", (item, value))
 
-        continue = input("\nWould you like to add another item or (q)uit? > ")
-        if coninue.lower() == "q":
+        cont = input("\nWould you like to add another item or (q)uit? > ")
+        if cont.lower() == "q":
             break
 
 
-def show_inventory():
-    pass
+def show_inventory(room):
+    total_value = 0
+    with access_db() as cursor:
+        cursor.execute("SELECT * FROM '" + room + "'")
+        print("\n")
+        for item in cursor:
+            print(f"{item[0]}, value: {item[1]}")
+            total_value += item[1]
+    print(f"\nTotal value of all items in room {room}: {total_value}")
+    return total_value
 
 
 def show_value():
-    pass
+    total_value = 0
+    for room in room_list():
+        total_value += show_inventory(room)
+    print(f"\nTotal value of all rooms: {total_value}")
 
 
 first_launch()
